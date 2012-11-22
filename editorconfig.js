17b96ebd347a4c34c03f48559238fa333b0a8fea
Trey Hunner <treyhunner@gmail.com>
2012-11-22 14:25:48 -0800

    Add basic buggy parser (proof of concept)

---
+++ editorconfig.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+var path = require('path');
+var minimatch = require('minimatch');
+var iniparser = require('iniparser');
+
+function getConfigFileNames(filepath) {
+  var old_dirname = filepath;
+  var dirname = old_dirname;
+  var paths = [];
+  do {
+    paths.push(path.join(dirname, ".editorconfig"));
+    old_dirname = dirname;
+    dirname = path.dirname(old_dirname);
+  } while(dirname != old_dirname);
+  return paths;
+}
+
+module.exports.parse = function(filepath) {
+
+  var dirname = path.dirname(filepath);
+  var filename = path.basename(filepath);
+  var filepaths = getConfigFileNames(dirname);
+  var matchOptions = {matchBase: true, dot: true, noext: true};
+  var configurations = [];
+  var matches = {};
+
+  for (var i in filepaths) {
+    var configFilePath = filepaths[i];
+    if (fs.existsSync(configFilePath)) {
+      var parsedOutput = iniparser.parseSync(configFilePath);
+      configurations.push([path.dirname(configFilePath), parsedOutput]);
+      if (parsedOutput.root == "true") break;
+    }
+  }
+
+  for (var j in configurations.reverse()) {
+    var pathPrefix = configurations[j][0];
+    var config = configurations[j][1];
+    for (var glob in config) {
+      var fullGlob = path.join(pathPrefix, "**/" + glob);
+      if (minimatch(filepath, fullGlob, matchOptions)) {
+        for (var k in config[glob]) {
+          matches[k] = config[glob][k];
+        }
+      }
+    }
+  }
+
+  return matches;
+
+};
