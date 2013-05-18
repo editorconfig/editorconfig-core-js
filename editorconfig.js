cd8c325efb2f1c050de4fd0b9a7d15e8b403627e
Jed Hunsaker <jed.hunsaker@gmail.com>
2013-05-18 16:48:08 -0700

    Add CLI support

--- editorconfig.js
+++ editorconfig.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var minimatch = require('./lib/fnmatch');
 var iniparser = require('./lib/ini');
+var Version = require('./lib/version');
 
 function getConfigFileNames(filepath) {
   var old_dirname = filepath;
@@ -15,19 +16,63 @@ function getConfigFileNames(filepath) {
   return paths;
 }
 
-module.exports.parse = function(filepath) {
+function processMatches(matches, version) {
+
+  // Set indent_size to "tab" if indent_size is unspecified and
+  // indent_style is set to "tab".
+  if ("indent_style" in matches && matches["indent_style"] === "tab" &&
+    !("indent_size" in matches) && version.gte(new Version(0, 10)))
+    matches["indent_size"] = "tab";
+
+  // Set tab_width to indent_size if indent_size is specified and
+  // tab_width is unspecified
+  if ("indent_size" in matches && !("tab_width" in matches) &&
+    matches["indent_size"] !== "tab")
+    matches["tab_width"] = matches["indent_size"];
+
+  // Set indent_size to tab_width if indent_size is "tab"
+  if("indent_size" in matches && "tab_width" in matches &&
+    matches["indent_size"] === "tab")
+    matches["indent_size"] = matches["tab_width"];
+
+  return matches;
+}
+
+function getOptions(options) {
+  if (typeof options === "undefined") {
+    options = {};
+  }
+  if (typeof options.version === "undefined") {
+    var package = JSON.parse(fs.readFileSync(__dirname + "/./package.json", "utf8"));
+    options.version = new Version(package.version);
+  }
+  return options;
+}
+
+module.exports.parse = function(filepath, options) {
 
   var filepaths = getConfigFileNames(path.dirname(filepath));
   var matchOptions = {matchBase: true, dot: true, noext: true};
   var configurations = [];
   var matches = {};
 
-  for (var i in filepaths) {
-    var configFilePath = filepaths[i];
-    if (fs.existsSync(configFilePath)) {
-      var parsedOutput = iniparser.parseSync(configFilePath);
-      configurations.push([path.dirname(configFilePath), parsedOutput]);
-      if (parsedOutput.root == "true") break;
+  var parsedOutput;
+  options = getOptions(options);
+  if (options.config) {
+    if (fs.existsSync(options.config)) {
+      parsedOutput = iniparser.parseSync(options.config);
+      configurations.push([path.dirname(options.config), parsedOutput]);
+    } else {
+      throw Error("Invalid configuration path");
+    }
+  } else {
+    for (var i in filepaths) {
+      var configFilePath = filepaths[i];
+      if (fs.existsSync(configFilePath)) {
+        parsedOutput = iniparser.parseSync(configFilePath);
+        configurations.push([path.dirname(configFilePath), parsedOutput]);
+        if (parsedOutput.root == "true") break;
+      }
     }
   }
 
@@ -38,12 +83,16 @@ module.exports.parse = function(filepath) {
       var fullGlob = path.join(pathPrefix, "**/" + glob);
       if (minimatch(filepath, fullGlob, matchOptions)) {
         for (var k in config[glob]) {
-          matches[k] = config[glob][k];
+          var value = config[glob][k].toLowerCase();
+          try {
+            value = JSON.parse(value);
+          } catch(e){}
+          matches[k] = value;
         }
       }
     }
   }
 
-  return matches;
+  return processMatches(matches, options.version);
 
 };
