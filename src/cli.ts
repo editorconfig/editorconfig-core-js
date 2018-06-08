ca729a61f7317e41d1434ca0a9b058a28a53c70d
Jed Mao <jedmao@outlook.com>
2018-06-08 01:38:01 -0500

    Update code to TypeScript 2.9 features

--- src/cli.ts
+++ src/cli.ts
@@ -3,8 +3,7 @@ import * as program from 'commander'
 
 import * as editorconfig from './'
 
-// tslint:disable-next-line:no-var-requires
-const pkg = require('./package.json')
+import pkg from '../package.json'
 
 export default function cli(args: string[]) {
   program.version('EditorConfig Node.js Core Version ' + pkg.version)
