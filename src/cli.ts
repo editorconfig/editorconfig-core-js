1f848c7b867edd0ed53c7d83016fdac9dcab8c22
Joe Hildebrand <joe-github@cursive.net>
2022-10-01 14:57:15 -0600

    Update dependencies.  Stub out lint for the moment.

--- src/cli.ts
+++ src/cli.ts
@@ -1,47 +1,47 @@
+/* eslint-disable no-console */
 // tslint:disable:no-console
-import program from 'commander'
+import { createCommand } from 'commander'
 
 import * as editorconfig from './'
 
 import pkg from '../package.json'
 
-export default function cli(args: string[]) {
-  program.version('EditorConfig Node.js Core Version ' + pkg.version)
+export default function cli(args: string[]): void {
+  const program = createCommand()
 
-  program
-    .usage([
-        '[OPTIONS] FILEPATH1 [FILEPATH2 FILEPATH3 ...]',
-        program._version,
-        'FILEPATH can be a hyphen (-) if you want path(s) to be read from stdin.',
-      ].join('\n\n  '))
-    .option('-f <path>',     'Specify conf filename other than \'.editorconfig\'')
-    .option('-b <version>',  'Specify version (used by devs to test compatibility)')
-    .option('-v, --version', 'Display version information')
+  program.version(
+    'EditorConfig Node.js Core Version ' + pkg.version,
+    '-v, --version',
+    'Display version information'
+  )
+    .showHelpAfterError()
+    .argument(
+      '<FILEPATH...>',
+      'Files to find configuration for.  Can be a hyphen (-) if you want path(s) to be read from stdin.'
+    )
+    .option('-f <path>',       'Specify conf filename other than \'.editorconfig\'')
+    .option('-b <version>',    'Specify version (used by devs to test compatibility)')
     .parse(args)
 
-  // Throw away the native -V flag in lieu of the one we've manually specified
-  // to adhere to testing requirements
-  program.options.shift()
-
   const files = program.args
+  const opts = program.opts()
 
-  if (!files.length) {
-    program.help()
-  }
-
-  files
-    .map((filePath) => editorconfig.parse(filePath, {
-      config: program.F,
-      version: program.B,
+  Promise.all(
+    files.map((filePath) => editorconfig.parse(filePath, {
+      config: opts.f as string,
+      version: opts.b as string,
     }))
-    .forEach((parsing, i, { length }) => {
-      parsing.then((parsed) => {
-        if (length > 1) {
-          console.log(`[${files[i]}]`)
-        }
-        Object.keys(parsed).forEach((key) => {
-          console.log(`${key}=${parsed[key]}`)
-        })
+  ).then((parsed) => {
+    const header = parsed.length > 1
+    parsed.forEach((props, i) => {
+      if (header) {
+        console.log(`[${files[i]}]`)
+      }
+      Object.keys(props).forEach((key) => {
+        console.log(`${key}=${String(props[key])}`)
       })
     })
+  }, er => {
+    console.error(er)
+  })
 }
